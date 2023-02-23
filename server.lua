9a0811b37688d3a217711143cefe8095cde5da2a
Tony Stark <76168122+tony-stark-17@users.noreply.github.com>
2023-02-23 21:46:50 +0530

    ESX

--- server.lua
+++ server.lua
@@ -15,9 +15,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ]]
-
-QBCore = exports['qb-core']:GetCoreObject()
-
+local ESX = exports['es_extended']:getSharedObject()
 local Encryptions = {}
 local charset = {}  do -- [0-9a-zA-Z]
     for c = 48, 57  do table.insert(charset, string.char(c)) end
@@ -31,30 +29,30 @@ local function randomString(length)
     return randomString(length - 1) .. charset[math.random(1, #charset)]
 end
 
-QBCore.Commands.Add("speedtest", "Do speed test", {{name = "model", help = "The model to speedtest"}}, true, function(source, args)
-    if #args < 1 then
-        TriggerClientEvent('QBCore:Notify', source, "Please pass arguments", "error")
-        return
-    end
+ESX.RegisterCommand({'speedtest'}, 'admin', function(xPlayer, args, showError)
+  if not args.vehicle then
+      TriggerClientEvent('esx:showNotification', source, "Please pass arguments", "error")
+      return
+  end
+
+  Encryptions[source] = randomString(16)
+  TriggerClientEvent("tis-vehspeeds:client:DoSpeedTest", source, Encryptions[source], args.vehicle)
+end, false, {help = 'Vehicle Speed Test', arguments = {{name = 'vehicle', help = 'Vehicle Model', type = 'any'}}})
+        
+ESX.RegisterCommand({'speedtestlist'}, 'admin', function(xPlayer, args, showError)
+  Encryptions[source] = randomString(16)
+  TriggerClientEvent("tis-vehspeeds:client:DoSpeedTestList", source, Encryptions[source])
+end, false, {help = 'Do speed test on the list in config.lua'})
+
+ESX.RegisterCommand({'speedtarget'}, 'admin', function(xPlayer, args, showError)
+  if not args.model or not args.target then
+     TriggerClientEvent('esx:showNotification', source, "Please pass arguments", "error")
+      return
+  end
 
     Encryptions[source] = randomString(16)
-    TriggerClientEvent("tis-vehspeeds:client:DoSpeedTest", source, Encryptions[source], args[1])
-end, "admin")
-
-QBCore.Commands.Add("speedtestlist", "Do speed test on the list in config.lua", {}, false, function(source, args)
-    Encryptions[source] = randomString(16)
-    TriggerClientEvent("tis-vehspeeds:client:DoSpeedTestList", source, Encryptions[source])
-end, "admin")
-
-QBCore.Commands.Add("speedtarget", "\"Train\" model for target speed", {{name = "model", help = "The model to speedtest"}, {name = "target", help = "Target 0-60mph (Float e.g.: 5.7)"}}, true, function(source, args)
-    if #args < 2 then
-        TriggerClientEvent('QBCore:Notify', source, "Please pass arguments", "error")
-        return
-    end
-
-    Encryptions[source] = randomString(16)
-    TriggerClientEvent("tis-vehspeeds:client:DoSpeedTarget", source, Encryptions[source], args[1], tonumber(args[2]))
-end, "admin")
+    TriggerClientEvent("tis-vehspeeds:client:DoSpeedTarget", source, Encryptions[source], args.model, args.target)
+end, false, {help = '"Train" model for target speed', arguments = {{name = 'model', help = 'the model to speedtest', type = 'any'}, {name = "target", help = "Target 0-60mph (Float e.g.: 5.7)", type = 'number'}}})
 
 RegisterServerEvent("tis-vehspeeds:server:SaveVehicleSpecs")
 AddEventHandler("tis-vehspeeds:server:SaveVehicleSpecs", function (encrypt, data)
